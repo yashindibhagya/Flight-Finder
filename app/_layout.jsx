6fa987f2ebd77d71fb12f7947aa61a3a145b8cc3
Yashindi Bhagya <yashindibhagya@gmail.com>
2025-10-28 20:13:48 +0530

    Refactor code structure for improved readability and maintainability

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -15,12 +15,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            title: "FlightFinder",
-          }}
-        />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,27 @@
-import { Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
-const index = () => {
+
+import { useRouter } from 'expo-router'
+import image from '../assets/images/loading_Image.png'
+
+export default function index() {
+
+    const router = useRouter()
     return (
-        <View>
-            <Text>index</Text>
+        <View style={styles.container}>
+            <Image source={image} style={styles.image} />
         </View>
     )
 }
 
-export default index
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
+})
