02ee4ddaa0e5532936e981341d6fbad35f46538b
Yashindi Bhagya <yashindibhagya@gmail.com>
2025-10-28 19:46:56 +0530

    Implement Firebase configuration and user detail context for authentication

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -15,8 +15,12 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "FlightFinder",
+          }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
